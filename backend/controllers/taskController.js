cbf71fbbf202718531180b67c77afb1b06aeea4d
a-agouzou <aagouzou1337@gmail.com>
2025-02-09 17:00:04 +0100

    Add task controllers for CRUD operations

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,17 @@
+import Task from '../models/Task.js';
+
+export const getTasks = async (req, res) => {
+    console.log('getTasks');
+}
+
+export const createTask = async (req, res) => {
+    console.log('createTask');
+}
+
+export const updateTask = async (req, res) => {
+    console.log('updateTask');
+}
+
+export const deleteTask = async (req, res) => {
+    console.log('deleteTask');
+}
