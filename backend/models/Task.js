53d8a677081c8dbfd7c16ec75d22bdacbe27ca94
a-agouzou <aagouzou1337@gmail.com>
2025-02-09 16:59:23 +0100

    Add Task model

---
+++ backend/models/Task.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+const taskSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ['Pending', 'Completed'],
+      default: 'Pending'
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  });
+  
+  const Task = mongoose.model('Task', taskSchema);
+  
+  export default Task;
